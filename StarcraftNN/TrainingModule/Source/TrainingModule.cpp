8d1dff277d2716d8cd0bfee5031aaed5ddad0aaf
Jacob Menashe <jmenashe@cs.utexas.edu>
2012-11-04 06:17:52 -0600

    Added a map and training module updates for training.

--- StarcraftNN/TrainingModule/Source/TrainingModule.cpp
+++ StarcraftNN/TrainingModule/Source/TrainingModule.cpp
@@ -1,21 +1,66 @@
 #include "TrainingModule.h"
 using namespace BWAPI;
 
-void ExampleAIModule::onStart()
-{
+void TrainingModule::onStart() {
   NEAT::Genome* g = new NEAT::Genome(5, 5, 0, 0);
-  Broodwar->sendText("Hello world!");
+  Broodwar->setLocalSpeed(5);
   Broodwar->printf("The map is %s, a %d player map",Broodwar->mapName().c_str(),Broodwar->getStartLocations().size());
 
-  if (!Broodwar->isReplay())
-  {
+  if (!Broodwar->isReplay()) {
     Broodwar->printf("The match up is %s v %s",
       Broodwar->self()->getRace().getName().c_str(),
       Broodwar->enemy()->getRace().getName().c_str());
   }
+  _frameCount = 0;
+  _maxAllyUnits = Broodwar->self()->getUnits().size();
+  _maxEnemyUnits = Broodwar->enemy()->getUnits().size();
+  _resetting = false;
 }
 
-void ExampleAIModule::onFrame()
-{
+void TrainingModule::onFrame() {
+  _frameCount++;
+	Position target(700,270);
+	if (Broodwar->getFrameCount()%30==0) {
+    for(std::set<Unit*>::const_iterator i=Broodwar->self()->getUnits().begin();i!=Broodwar->self()->getUnits().end();i++) {
+      Unit* unit = *i;
+      if(unit->getType().isFlyer()) continue;
+      unit->attack(target);
+      Position p = unit->getPosition();
+    }
+  }
+}
 
+void TrainingModule::onUnitCreate(BWAPI::Unit* unit) {
+  _resetting = false;
+}
+
+void TrainingModule::onUnitDestroy(BWAPI::Unit* unit) {
+  if(_resetting) return;
+  if(unit->getType().isFlyer()) return;
+  int survivors = Broodwar->self()->getUnits().size();
+  int opponents = Broodwar->enemy()->getUnits().size();
+  if(survivors == 0 || opponents == 0) {
+    finishRound();
+  }
+}
+
+void TrainingModule::finishRound() {
+  _resetting = true;
+  int survivors = Broodwar->self()->getUnits().size();
+  int opponents = Broodwar->enemy()->getUnits().size();
+  int frames = _frameCount;
+  _frameCount = 0;
+  double score = survivors - opponents;
+  score *= 200.0 / frames;
+  Broodwar->printf("%i survivors, %i opponents, %i frames, score of %2.4f", survivors, opponents, frames, score);
+}
+
+void TrainingModule::onEnd(bool isWinner) {
+  saveGenomes();
+}
+
+void TrainingModule::loadGenomes() {
+}
+
+void TrainingModule::saveGenomes() {
 }
--- StarcraftNN/TrainingModule/Source/TrainingModule.h
+++ StarcraftNN/TrainingModule/Source/TrainingModule.h
@@ -5,9 +5,18 @@
 #include <windows.h>
 #include <genome.h>
 
-class ExampleAIModule : public BWAPI::AIModule
-{
-public:
-  virtual void onStart();
-  virtual void onFrame();
+class TrainingModule : public BWAPI::AIModule {
+  public:
+    virtual void onStart();
+    virtual void onFrame();
+    virtual void onUnitDestroy(BWAPI::Unit*);
+    virtual void onUnitCreate(BWAPI::Unit*);
+    virtual void onEnd(bool);
+    void finishRound();
+    void loadGenomes();
+    void saveGenomes();
+  private:
+    int _frameCount;  
+    int _maxEnemyUnits, _maxAllyUnits;
+    bool _resetting;
 };
