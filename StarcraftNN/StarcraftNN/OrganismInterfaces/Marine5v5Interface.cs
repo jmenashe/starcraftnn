524719398d529affcbf76e4e0e1b36cb208eb204
U-Keshari-PC\Vineet <Vineet@Keshari-PC.(none)>
2012-12-09 03:29:07 -0600

    Added maps, squad interface

---
+++ StarcraftNN/StarcraftNN/OrganismInterfaces/Marine5v5Interface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarcraftNN.OrganismInterfaces
+{
+    public class Marine5v5Interface : IndividualControlInterface
+    {
+    }
+}
---
+++ StarcraftNN/StarcraftNN/OrganismInterfaces/Squads12v12.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarcraftNN.OrganismInterfaces
+{
+    public class Squads12v12 : SquadControlInterface
+    {
+        public override int UnitCount
+        {
+            get { return 12; }
+        }
+    }
+}
